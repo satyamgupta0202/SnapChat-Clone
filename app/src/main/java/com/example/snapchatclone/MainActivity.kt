6ae62c8c1f0afb5c636210b0bd275c1dc2caf3c0
satyam <2019335@iiitdmj.ac.in.com>
2021-03-09 13:04:18 +0530

    LoginAdded

--- app/src/main/java/com/example/snapchatclone/MainActivity.kt
+++ app/src/main/java/com/example/snapchatclone/MainActivity.kt
@@ -1,12 +1,73 @@
 package com.example.snapchatclone
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R.attr.password
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+
 
 class MainActivity : AppCompatActivity() {
+     var passwordEditText: EditText ? = null
+     var emailEditText: EditText ?= null
+     val mAuth = FirebaseAuth.getInstance()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        passwordEditText = findViewById(R.id.passwordEditText)
+        emailEditText = findViewById(R.id.emailEditText)
+
+        if (mAuth.currentUser != null) {
+            login()
+        }
     }
+
+
+        fun goClicked(view: View){
+
+            /// check if already user else signup
+            mAuth.signInWithEmailAndPassword(emailEditText.toString(), passwordEditText.toString())
+                .addOnCompleteListener(
+                    this
+                ) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        login()
+                    } else {
+                        // If sign in fails, display a message to the user.
+                         mAuth.createUserWithEmailAndPassword(emailEditText.toString(), passwordEditText.toString()).addOnCompleteListener(this,
+                         ) { task ->
+
+                             if(task.isSuccessful){
+                                 //adds user to database
+                                 login()
+                             }
+                             else{
+                                 Toast.makeText(this,"Login FialedTry Ahain",Toast.LENGTH_SHORT).show()
+                             }
+                         }
+                        // ...
+                    }
+
+                    // ...
+                }
+
+        }
+
+
+        fun login(){
+                // new page
+           val intent = Intent(this,SnapsActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
 }
