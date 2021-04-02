625d1d9914289561eda3083a11212cfcc7702f45
satyam <2019335@iiitdmj.ac.in.com>
2021-04-02 22:54:03 +0530

    MenuBarAdded

--- app/src/main/java/com/example/snapchatclone/CreateSnapsActivity2.kt
+++ app/src/main/java/com/example/snapchatclone/CreateSnapsActivity2.kt
@@ -1,11 +1,26 @@
 package com.example.snapchatclone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import java.util.jar.Manifest
 
 class CreateSnapsActivity2 : AppCompatActivity() {
+    var createSnapImageview : ImageView? = null
+    var messageEditText : EditText? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_snaps2)
+    }
+    fun getphoto(){
+        val intent  = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent,1)
+    }
+    fun uploadImage(view: View){
+        if(checkSelfPermission(Manifest.permission.ReadExternalStorage))
     }
 }
