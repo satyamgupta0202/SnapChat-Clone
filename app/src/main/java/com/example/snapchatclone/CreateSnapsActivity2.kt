6da9d0cc1b7f519f7f25cd8957dfca8a1e525166
satyam <2019335@iiitdmj.ac.in.com>
2021-04-03 12:19:55 +0530

    Image_Processed

--- app/src/main/java/com/example/snapchatclone/CreateSnapsActivity2.kt
+++ app/src/main/java/com/example/snapchatclone/CreateSnapsActivity2.kt
@@ -1,13 +1,18 @@
 package com.example.snapchatclone
 
+import android.app.Activity
+import android.content.ContentResolver
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
 import android.widget.EditText
 import android.widget.ImageView
+import java.lang.Exception
 import java.util.jar.Manifest
 
 class CreateSnapsActivity2 : AppCompatActivity() {
@@ -28,6 +33,28 @@ class CreateSnapsActivity2 : AppCompatActivity() {
             requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1)
         } else {
             getphoto()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val selectedImage = data!!.data
+        if(requestCode==1 && resultCode== Activity.RESULT_OK && data!=null){
+            try {
+                    val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver,selectedImage)
+                createSnapImageview ?.setImageBitmap(bitmap)
+
+            }
+            catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode==1){
+            if(grantResults.size>0 &&)
         }
     }
 
