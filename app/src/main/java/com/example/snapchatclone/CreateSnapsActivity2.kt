327436ae3cbee1a806688dd1214f1d075093db31
satyam <2019335@iiitdmj.ac.in.com>
2021-04-03 11:20:59 +0530

    Image_Processed

--- app/src/main/java/com/example/snapchatclone/CreateSnapsActivity2.kt
+++ app/src/main/java/com/example/snapchatclone/CreateSnapsActivity2.kt
@@ -1,6 +1,7 @@
 package com.example.snapchatclone
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -10,17 +11,26 @@ import android.widget.ImageView
 import java.util.jar.Manifest
 
 class CreateSnapsActivity2 : AppCompatActivity() {
-    var createSnapImageview : ImageView? = null
-    var messageEditText : EditText? = null
+    var createSnapImageview: ImageView? = null
+    var messageEditText: EditText? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_snaps2)
     }
-    fun getphoto(){
-        val intent  = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-        startActivityForResult(intent,1)
+
+    fun getphoto() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, 1)
     }
-    fun uploadImage(view: View){
-        if(checkSelfPermission(Manifest.permission.ReadExternalStorage))
+
+    fun uploadImage(view: View) {
+        if (checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1)
+        } else {
+            getphoto()
+        }
     }
+
 }
+
+        
